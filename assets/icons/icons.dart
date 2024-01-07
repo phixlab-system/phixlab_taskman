79c7150d1c7c7ae7adb5325ac70104252cbbc1e2
Philips Nge <5889921+sudophils@users.noreply.github.com>
2024-01-07 10:18:47 -0500

    icons

---
+++ assets/icons/icons.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+/// Icons menu items
+
+//1. in asset/icons folder : dashboard.png
+//2. All Task: Icon.list_alt
+//3. Report: Icons.pie_chart
+//4. in assets/icons folder : integration.png
+//5. export: Icons.download_for_offline_rounded
+
+/// Icons for dashboard cards
+
+//1. Todo : Icon.list
+//2. Task completed : Icons.check_circle
+//3. In review: Icons.loop
+//4. In review: Icons.info
+
+const String dashBoardIconFile = 'assets/icons/dashboard.png';
+const String integrationIconFile = 'assets/icons/integration.png';
+
--- lib/resources/color/color.dart
+++ lib/resources/color/color.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 const Color primaryColor = Color(0xFF5051f9);
 const Color fontColorDark = Color(0xFF0b0a4d);
 const Color fontColorLight = Color(0xFF798498);
+const Color iconInActiveColor = Color(0xFF778397);
 
 
 
@@ -10,4 +11,6 @@ const Color lightGreen = Color(0xFFd2eedc);
 const Color lightYellow = Color(0xFFfeeccf);
 const Color lightRed = Color(0xFFf9d5d5);
 const Color lightBlue = Color(0xFFe9f0fb);
+
+
 
